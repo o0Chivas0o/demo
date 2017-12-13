426a57e8dd5689b160add2064b914bee0ccd0980
Lee <326656669@qq.com>
2017-12-13 18:11:27 +0800

    bash修改 添加回车

--- demo.sh
+++ demo.sh
@@ -5,9 +5,9 @@ else
   mkdir $1
   cd $1
   mkdir css js
-  touch index.html && echo "<!DOCTYPE>;<title>Hello</title>;<h1>hi</h1>" > index.html
+  touch index.html && echo "<!DOCTYPE>/n <title>Hello</title>/n <h1>hi</h1>" > index.html
   touch css/style.css && echo "h1{color:red}" > css/style.css
-  touch js/main.js && echo "var string = 'Hello World';alert(string)" > js/main.js
+  touch js/main.js && echo "var string = 'Hello World'/n alert(string)" > js/main.js
   echo 'success'
   exit
 fi
