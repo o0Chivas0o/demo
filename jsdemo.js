48ac6d4db9ef14164411cd416cc551e8af2c9682
Lee <326656669@qq.com>
2017-12-13 18:12:51 +0800

    node脚本修改 添加回车

--- jsdemo.js
+++ jsdemo.js
@@ -8,8 +8,8 @@
  fs.mkdirSync('css') // mkdir css
  fs.mkdirSync('js') // mkdir js
 
- fs.writeFileSync("./index.html", " <!DOCTYPE>;<title>Hello</title>;<h1>Hi</h1>")
+ fs.writeFileSync("./index.html", " <!DOCTYPE>/n <title>Hello</title>/n <h1>Hi</h1>")
  fs.writeFileSync("css/style.css", "h1{color: red;}")
- fs.writeFileSync("./js/main.js", " var string = 'Hello World';alert(string)")
+ fs.writeFileSync("./js/main.js", " var string = 'Hello World' /n alert(string)")
 
  process.exit(0)
