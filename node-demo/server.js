d88fafcd5ccab8cb1013d84bd6c1beb7b1dd1ef2
Lee <326656669@qq.com>
2017-12-14 21:51:50 +0800

    node.js 创建一个服务器

--- node-demo/server.js
+++ node-demo/server.js
@@ -2,12 +2,10 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
-
 if(!port){
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
-
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
   var path = request.url 
@@ -16,34 +14,30 @@ var server = http.createServer(function(request, response){
   var pathNoQuery = parsedUrl.pathname
   var queryObject = parsedUrl.query
   var method = request.method
-
-  /******** 从这里开始看，上面不要看 ************/
-
   console.log('HTTP 路径为\n' + path)
-  if(path == '/style.js'){
-    response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('body{background-color: #ddd;}h1{color: red;}')
-    response.end()
-  }else if(path == '/script.html'){
-    response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-    response.write('alert("这是JS执行的")')
-    response.end()
-  }else if(path == '/index.css'){
-    response.setHeader('Content-Type', 'text/html; charset=utf-8')
-    response.write('<!DOCTYPE>\n<html>'  + 
-      '<head><link rel="stylesheet" href="/style.js">' +
-      '</head><body>'  +
+  if(path == '/style.css'){
+    response.setHeader('Content-type','text/css;charset=utf-8')
+    response.write('body{background:orange;}h1{color:red;}')
+    response.end() 
+  }else if(path == '/'){
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    response.write('<!DOCTYPE>\n<html>' + 
+      '<head><link rel="stylesheet" href="/style.css">' + 
+      '</head><body>' + 
       '<h1>你好</h1>' +
-      '<script src="/script.html"></script>' +
-      '</body></html>')
+      '<script src="/main.js"></script>' + 
+      '</body></html>');
+    response.end()
+  }else if(path == '/main.js'){
+    response.setHeader('Content-Type','text/javascript;charset=utf-8')
+    response.write('alert("这是js执行的")')
     response.end()
   }else{
     response.statusCode = 404
     response.end()
   }
-
-  /******** 代码结束，下面不要看 ************/
 })
-
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
