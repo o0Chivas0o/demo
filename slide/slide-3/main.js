83adde5310cf97b799d389fb55fe5739121dcc3a
Lee <326656669@qq.com>
2018-01-24 10:30:42 +0800

    更改为current,enter,leave三个状态,同时用js切换状态

--- slide/slide-3/main.js
+++ slide/slide-3/main.js
@@ -1,44 +1,27 @@
-setTimeout(()=>{
-  $('.image >img:nth-child(1)').css({
-    transform:'translateX(-100%)'
+$('.image >img:nth-child(1)').addClass('current')
+$('.image >img:nth-child(2)').addClass('enter')
+$('.image >img:nth-child(3)').addClass('enter')
+setTimeout(() => {
+  $('.image >img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
+    $(e.currentTarget).removeClass('leave').addClass('enter')
   })
-  $('.image >img:nth-child(2)').css({
-    transform:'translateX(-100%)'
+  $('.image >img:nth-child(2)').removeClass('enter').addClass('current')
+}, 1000)
+setTimeout(() => {
+  $('.image >img:nth-child(2)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
+    $(e.currentTarget).removeClass('leave').addClass('enter')
   })
-  $('.image >img:nth-child(1)').one('transitionend',(e)=>{
-    $(e.currentTarget).addClass('right').css({transform:'none'})
+  $('.image >img:nth-child(3)').removeClass('enter').addClass('current')
+}, 2000)
+setTimeout(() => {
+  $('.image >img:nth-child(3)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
+    $(e.currentTarget).removeClass('leave').addClass('enter')
   })
-},1000)
-setTimeout(()=>{
-  $('.image >img:nth-child(2)').css({
-    transform:'translateX(-200%)'
+  $('.image >img:nth-child(1)').removeClass('enter').addClass('current')
+}, 3000)
+setTimeout(() => {
+  $('.image >img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
+    $(e.currentTarget).removeClass('leave').addClass('enter')
   })
-  $('.image >img:nth-child(3)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.image >img:nth-child(2)').one('transitionend',(e)=>{
-    $(e.currentTarget).addClass('right').css({transform:'none'})
-  })
-},2000)
-setTimeout(()=>{
-  $('.image >img:nth-child(3)').css({
-    transform:'translateX(-200%)'
-  })
-  $('.image >img:nth-child(1)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.image >img:nth-child(3)').one('transitionend',(e)=>{
-    $(e.currentTarget).addClass('right').css({transform:'none'})
-  })
-},3000)
-setTimeout(()=>{
-  $('.image >img:nth-child(2)').css({
-    transform:'translateX(-200%)'
-  })
-  $('.image >img:nth-child(3)').css({
-    transform:'translateX(-100%)'
-  })
-  $('.image >img:nth-child(1)').one('transitionend',(e)=>{
-    $(e.currentTarget).addClass('right').css({transform:'none'})
-  })
-},4000)
+  $('.image >img:nth-child(2)').removeClass('enter').addClass('current')
+}, 4000)
