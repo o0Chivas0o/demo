5ab5fddc3706d7b8709b38fb8cdbd4ea668a06e6
Lee <326656669@qq.com>
2018-01-24 10:57:12 +0800

    修改JS

--- slide/slide-3/main.js
+++ slide/slide-3/main.js
@@ -1,27 +1,22 @@
-$('.image >img:nth-child(1)').addClass('current')
-$('.image >img:nth-child(2)').addClass('enter')
-$('.image >img:nth-child(3)').addClass('enter')
-setTimeout(() => {
-  $('.image >img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
+$('.image > img:nth-child(1)').addClass('current')
+$('.image > img:nth-child(2)').addClass('enter')
+$('.image > img:nth-child(3)').addClass('enter')
+let n = 1
+setInterval(() => {
+  $(`.image > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    .one('transitionend', (e) => {
     $(e.currentTarget).removeClass('leave').addClass('enter')
   })
-  $('.image >img:nth-child(2)').removeClass('enter').addClass('current')
-}, 1000)
-setTimeout(() => {
-  $('.image >img:nth-child(2)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
-    $(e.currentTarget).removeClass('leave').addClass('enter')
-  })
-  $('.image >img:nth-child(3)').removeClass('enter').addClass('current')
-}, 2000)
-setTimeout(() => {
-  $('.image >img:nth-child(3)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
-    $(e.currentTarget).removeClass('leave').addClass('enter')
-  })
-  $('.image >img:nth-child(1)').removeClass('enter').addClass('current')
+  $(`.image > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+  n += 1
 }, 3000)
-setTimeout(() => {
-  $('.image >img:nth-child(1)').removeClass('current').addClass('leave').one('transitionend',(e)=>{
-    $(e.currentTarget).removeClass('leave').addClass('enter')
-  })
-  $('.image >img:nth-child(2)').removeClass('enter').addClass('current')
-}, 4000)
+
+function x (n) {
+  if (n > 3) {
+    n = n % 3
+    if (n === 0) {
+      n = 3
+    }
+  }
+  return n
+}
