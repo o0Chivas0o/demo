78d297e0d4cdd8e6a97a89aabdea7ea891157e9f
Lee <326656669@qq.com>
2018-01-25 20:18:43 +0800

    修改JS 使点击浮层不消失

--- popover/main.js
+++ popover/main.js
@@ -1,19 +1,20 @@
 // 第一种方式实现popover
-// clickMe.addEventListener('click',function(e){
-//   popover.classList.add('active')
-//   e.stopPropagation()
-// })
-//
-// document.documentElement.addEventListener('click',function(){
-//   popover.classList.remove('active')
-// })
-
-// 第二种方法实现
-$(clickMe).on('click',()=>{
-  $(popover).addClass('active')
-  setTimeout(()=>{
-    $(document).one('click',()=>{
-      $(popover).removeClass('active')
-    })
-  })
+clickMe.addEventListener('click', function (e) {
+  popover.classList.add('active')
 })
+wrapper.addEventListener('click', function (e) {
+  e.stopPropagation()
+})
+document.documentElement.addEventListener('click', function () {
+  popover.classList.remove('active')
+})
+
+// 第二种方法实现 jquery
+// $(clickMe).on('click',()=>{
+//   $(popover).addClass('active')
+//   setTimeout(()=>{
+//     $(document).one('click',()=>{
+//       $(popover).removeClass('active')
+//     })
+//   })
+// })
