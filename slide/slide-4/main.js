404cdd0a63704467de4a082e00e9312e25e06f3a
Lee <326656669@qq.com>
2018-01-26 15:40:21 +0800

    添加上一张 下一张按钮,添加自动播放,优化代码 有一个BUG 动画未结束就点击下一张会产生动画错乱

--- slide/slide-4/main.js
+++ slide/slide-4/main.js
@@ -3,52 +3,100 @@ let $slides = $('#slide')
 let $images = $slides.children('img')
 let current = 0
 
-makeFakeSlides($images,$slides)
+makeFakeSlides()
 $slides.css({transform: 'translateX(-300px)'})
-bindEvents($buttons)
+bindEvents()
 
-function bindEvents ($buttons) {
-  $buttons.eq(0).on('click', () => {
-    console.log(current)
-    if (current === 2) {
-      console.log('最后一张到第一张')
-      $slides.css({transform: 'translateX(-1200px)'})
-        .one('transitionend', () => {
-          $slides.hide()
-            .offset() // 阻止浏览器把hide() 和 show() 合并
-                      // offset()返回的是一个对象无法链式操作
-          $slides.css({transform: 'translateX(-300px)'}).show()
-        })
-    } else {
-      $slides.css({transform: 'translateX(-300px)'})
-      current = 0
-    }
+$(pre).on('click',()=>{
+  goToSlide(current - 1)
+})
+$(next).on('click',()=>{
+  goToSlide(current + 1)
+})
+
+let timer = setInterval(()=>{
+  goToSlide(current+1)
+},1500)
+
+$('.container').on('mouseenter',()=>{
+  window.clearInterval(timer)
+}).on('mouseleave',()=>{
+  timer = setInterval(()=>{
+    goToSlide(current+1)
+  },1500)
+})
+
+function bindEvents () {
+  $('.buttons').on('click', 'button', (e) => {
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goToSlide(index)
   })
-  $buttons.eq(1).on('click', () => {
-    console.log(current)
-    $slides.css({transform: 'translateX(-600px)'})
-    current = 1
-  })
-  $buttons.eq(2).on('click', () => {
-    if (current === 0) {
-      console.log(current)
-      console.log('第一张到最后一张')
-      $slides.css({transform: 'translateX(0)'})
-        .one('transitionend', () => {
-          $slides.hide()
-            .offset()
-          $slides.css({transform: 'translateX(-900px)'}).show()
-        })
-    } else {
-      $slides.css({transform: 'translateX(-900px)'})
-      current = 2
-    }
-  })
+
+  // $buttons.eq(0).on('click', () => {
+  //   console.log(current)
+  //   if (current === 2) {
+  //     console.log('最后一张到第一张')
+  //     $slides.css({transform: 'translateX(-1200px)'})
+  //       .one('transitionend', () => {
+  //         $slides.hide()
+  //           .offset() // 阻止浏览器把hide() 和 show() 合并
+  //                     // offset()返回的是一个对象无法链式操作
+  //         $slides.css({transform: 'translateX(-300px)'}).show()
+  //       })
+  //   } else {
+  //     $slides.css({transform: 'translateX(-300px)'})
+  //     current = 0
+  //   }
+  // })
+  // $buttons.eq(1).on('click', () => {
+  //   console.log(current)
+  //   $slides.css({transform: 'translateX(-600px)'})
+  //   current = 1
+  // })
+  // $buttons.eq(2).on('click', () => {
+  //   if (current === 0) {
+  //     console.log(current)
+  //     console.log('第一张到最后一张')
+  //     $slides.css({transform: 'translateX(0)'})
+  //       .one('transitionend', () => {
+  //         $slides.hide()
+  //           .offset()
+  //         $slides.css({transform: 'translateX(-900px)'}).show()
+  //       })
+  //   } else {
+  //     $slides.css({transform: 'translateX(-900px)'})
+  //     current = 2
+  //   }
+  // })
 }
 
-function makeFakeSlides ($images,$slides) {
+function makeFakeSlides () {
   let $firstCopy = $images.eq(0).clone(true) // 复制第一张
   let $lastCopy = $images.eq($images.length - 1).clone(true) // 复制最后一张
   $slides.append($firstCopy) // 把第一张放到最后一张后面
   $slides.prepend($lastCopy) // 把最后一张放到第一张前面
+}
+function goToSlide(index){
+  if(index>$buttons.length-1){
+    index = 0
+  }else if(index<0){
+    index = $buttons.length -1
+  }
+  if (current === $buttons.length - 1 && index === 0) {
+    $slides.css({transform: `translateX(${-($buttons.length + 1) * 300}px)`})
+      .one('transitionend', () => {
+        $slides.hide().offset()
+        $slides.css({transform: `translateX(${-(index + 1) * 300}px)`}).show()
+      })
+  } else if (current === 0 && index === $buttons.length - 1) {
+    $slides.css({transform: `translateX(0px)`})
+      .one('transitionend', () => {
+        $slides.hide().offset()
+        $slides.css({transform: `translateX(${-(index + 1) * 300}px)`}).show()
+      })
+  } else {
+    $slides.css({transform: `translateX(${-(index + 1) * 300}px)`})
+  }
+  current = index
 }
