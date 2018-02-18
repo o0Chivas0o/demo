278de57dbec0641b0a41ca86f211bbb0a0927888
Lee <326656669@qq.com>
2018-02-18 11:33:33 +0800

    sign_up demo 以及 错误提示

--- node-demo/sign-up-demo/server.js
+++ node-demo/sign-up-demo/server.js
@@ -1,45 +1,76 @@
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
+let http = require('http')
+let fs = require('fs')
+let url = require('url')
+let port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
-  var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url
-  var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-  var path = parsedUrl.pathname
-  var query = parsedUrl.query
-  var method = request.method
+let server = http.createServer(function (request, response) {
+  let parsedUrl = url.parse(request.url, true)
+  let pathWithQuery = request.url
+  let queryString = ''
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  let path = parsedUrl.pathname
+  let query = parsedUrl.query
+  let method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     let string = fs.readFileSync('./index.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path==='/sign_up'){
+  } else if (path === '/sign_up' && method === 'GET') {
     let string = fs.readFileSync('./sign_up.html', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path==='/main.js'){
+  } else if (path === '/sign_up' && method === 'POST') {
+    readBody(request).then(
+      (body) => {
+        let strings = body.split('&') // ['email=','password=','password_confirmation=']
+        let hash = {}
+        strings.forEach((string) => {
+          // string === 'email='
+          let parts = string.split('=')
+          let key = parts[0]
+          hash[key] = parts[1] //hash['email'] = 'value'
+        })
+        // let email = hash['email']
+        // let password = hash['password']
+        // let password_confirmation = hash['password_confirmation']
+        let {email, password, password_confirmation} = hash
+        if (email.indexOf('@') === -1) {
+          response.statusCode = 400
+
+          response.write(`{
+          "errors":{
+            "email":"invalid"
+            }
+          }`)
+        } else if (password !== password_confirmation) {
+          response.statusCode = 400
+          response.write('password not match')
+        } else {
+          response.statusCode = 200
+        }
+        response.end()
+      })
+  } else if (path === '/main.js') {
     let string = fs.readFileSync('./main.js', 'utf8')
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path==='/xxx'){
+  } else if (path === '/xxx') {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/json;charset=utf-8')
     response.setHeader('Access-Control-Allow-Origin', 'http://jack.com:8002')
@@ -54,7 +85,7 @@ var server = http.createServer(function(request, response){
     }
     `)
     response.end()
-  }else{
+  } else {
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(`
@@ -67,6 +98,18 @@ var server = http.createServer(function(request, response){
 
   /******** 代码结束，下面不要看 ************/
 })
+
+function readBody (request) {
+  return new Promise((resolve, reject) => {
+    let body = [] // 请求体
+    request.on('data', (chunk) => {
+      body.push(chunk)
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      resolve(body)
+    })
+  })
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
